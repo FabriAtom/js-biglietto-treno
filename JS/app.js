fc7f7790cbe2f552bdfc76a8a9f70deee6376fa0
FabriAtom <fabrizioromano20@gmail.com>
2022-07-20 17:58:29 +0200

    document

--- JS/app.js
+++ JS/app.js
@@ -1,32 +1,19 @@
-const prezzo = 0.21
-const percentualeSconto = 0.2
-const sconto = prezzo * percentualeSconto
-const prezzoScontato = prezzo - sconto
-
-console.log(prezzo, percentualeSconto, sconto, prezzoScontato);
-
-
-
+const tariffa = 0.21;
 
 const numeroKm = prompt(' si prega di inserire il numero di chilometri che deve percorrere');
-// if (numeroKm === 0.21) {
-
-    
-// }
-
-
-
 
 const numeroEta = prompt("si prega di inserire l'eta del passeggero");
+
+var prezzo = tariffa * numeroKm;
+console.log('prezzo1: ' + prezzo);
+
 if (numeroEta < 18) {
-    ((prezzo / 100) * 80);
-    
+    prezzo = prezzo - (prezzo * 0.2);
 
 
 }else if (numeroEta > 65) {
-    ((prezzo / 100) * 60);
-
-
-}else {
-
+    prezzo = prezzo - (prezzo * 0.4);
 }
+console.log(prezzo);
+
+document.getElementById('id').innerText = numeroKm + numeroEta;
